94b186e06f86145fada84560d3aa803966d02e7f
Ethan.OToole <eotoole005@gmail.com>
2025-04-15 13:09:47 -0400

    Completed HighScoreWins

--- HighScoreWins/src/main/java/com/pluralsight/HighScoreWinsApp.java
+++ HighScoreWins/src/main/java/com/pluralsight/HighScoreWinsApp.java
@@ -11,13 +11,20 @@ public class HighScoreWinsApp {
 
         System.out.print("Please enter a game score (Ex: Home:Visitor|5:4): ");
         userInput = input.nextLine();
-        String[] scores = userInput.split("\\|");
-        String[] names = userInput.split(":");
-        teamOne = names[0];
-        teamTwo = names[1];
-        scoreOne = Integer.parseInt(names[2]);
-        scoreTwo = Integer.parseInt(names[3]);
+        //Split input into two parts one for names and the other for scores.
+        String[] inputParts = userInput.split("\\|");
 
+        //Split the inputParts now by name and scores.
+        String[] teamNames = inputParts[0].split(":");
+        String[] scores = inputParts[1].split(":");
+
+        //Assign those values for teamName and scores.
+        teamOne = teamNames[0];
+        teamTwo = teamNames[1];
+        scoreOne = Integer.parseInt(scores[0]);
+        scoreTwo = Integer.parseInt(scores[1]);
+
+        //Compare to see who is the winner or if there is a tie.
         if(scoreOne > scoreTwo) {
             System.out.println("Winner: " + teamOne);
         } else if (scoreOne < scoreTwo) {
